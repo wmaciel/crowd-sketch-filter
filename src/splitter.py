cb9c6b14a4bd2398ea7936d4748d1f4a9e48a988
Walther Maciel <walthermaciel@gmail.com>
2016-11-20 11:44:07 -0800

    linting

--- src/splitter.py
+++ src/splitter.py
@@ -1,38 +1,44 @@
-from PIL import Image
+import sys
 import os
+from PIL import Image
 
 
-# Generates the limits of a crop box in a single dimension
-# Example, if dim is 512 pixels and num_divs is 5, it should return:
-# [(0, 103), (103, 206), (206, 308), (308, 410), (410, 512)]
 def generate_1d_box_limits(dim, num_divs):
+    '''
+    Generates the limits of a crop box in a single dimension
+    Example, if dim is 512 pixels and num_divs is 5, it should return:
+    [(0, 103), (103, 206), (206, 308), (308, 410), (410, 512)]
+    '''
     assert dim >= num_divs
 
     div_size = dim / num_divs
     missing_pixels = dim % num_divs
     boxes = []
-    d0 = 0
+    d_0 = 0
 
-    while d0 < dim:
-        d1 = d0 + div_size
+    while d_0 < dim:
+        d_1 = d_0 + div_size
 
         if missing_pixels > 0:
-            d1 += 1
+            d_1 += 1
             missing_pixels -= 1
 
-        boxes.append((d0, d1))
-        d0 = d1
+        boxes.append((d_0, d_1))
+        d_0 = d_1
 
     return boxes
 
 
 def split_image(file_path, n_x, n_y, out_folder):
+    '''
+    Splits the image from file_path into n_x * n_y pieces and saves them inside out_folder
+    '''
     os.mkdir(out_folder)
     original = Image.open(file_path)
-    w, h = original.size
 
-    boxes_x = generate_1d_box_limits(w, n_x)
-    boxes_y = generate_1d_box_limits(h, n_y)
+    original_width, original_height = original.size
+    boxes_x = generate_1d_box_limits(original_width, n_x)
+    boxes_y = generate_1d_box_limits(original_height, n_y)
 
     # Merge 1d boxes into 2d boxes
     boxes = []
@@ -41,7 +47,7 @@ def split_image(file_path, n_x, n_y, out_folder):
             box = (box_x[0], box_y[0], box_x[1], box_y[1])
             boxes.append(box)
 
-    for i, box in enumerate(boxes):
+    for box in boxes:
         region = original.crop(box)
         filename = str(box[0]) + '_' + str(box[1]) + '_' + str(box[2]) + '_' + str(box[3]) + '.bmp'
         out_path = os.path.join(out_folder, filename)
@@ -49,4 +55,4 @@ def split_image(file_path, n_x, n_y, out_folder):
 
 
 if __name__ == '__main__':
-    split_image('lena.bmp', 5, 5, 'lena_divs')
+    split_image(sys.argv[1], 5, 5, sys.argv[2])
--- src/stitcher.py
+++ src/stitcher.py
@@ -1,15 +1,21 @@
-from PIL import Image
 import os
+from PIL import Image
 
 def stitch_images(original_path, folder_path, out_path):
+    '''
+    Stitches images back into its original locations
+    Uses the image at original_path as a backdrop
+    Pastes the image pieces from folder_path
+    Saves the new image into out_path
+    '''
     original = Image.open(original_path)
 
     file_list = os.listdir(folder_path)
-    for f in file_list:
-        file_name, file_ext = os.path.splitext(f)
+    for file_path in file_list:
+        file_name = os.path.splitext(file_path)[0]
         box = tuple(map(int, file_name.split('_')))
 
-        region = Image.open(os.path.join(folder_path, f))
+        region = Image.open(os.path.join(folder_path, file_path))
         original.paste(region, box)
 
     original.save(out_path)
