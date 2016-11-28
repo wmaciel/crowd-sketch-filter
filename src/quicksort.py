7947f56d471c75f12b52cc2d3f9f42e0e45a5ced
loongchan <loongchan@gmail.com>
2016-11-28 11:31:35 -0800

    quicksort.py from assignment one example.

---
+++ src/quicksort.py
@@ -0,0 +1,54 @@
+import random
+from reprowd.crowdcontext import *
+from reprowd.presenter.image import ImageCmp
+
+/* credit to https://github.com/sfu-db/reprowd/blob/master/reprowd/examples/quicksort.py */
+def _quicksort(crowddata, object_list, orig_img):
+    if len(object_list) <= 1:
+        return crowddata
+
+    pivot_index = random.randint(0, len(object_list)-1)
+    pivot = object_list.pop(pivot_index)
+
+    object_pair_list = [(orig_img, o, pivot) for o in object_list]
+    crowddata = crowddata.clear().extend(object_pair_list).publish_task().get_result().quality_control("mv")
+
+    left = []
+    right = []
+    for obj, result in zip(crowddata.data['object'], crowddata.data['mv']):
+        if result == 'right':
+            left.append(obj[0])
+        else:
+            right.append(obj[0])
+
+    crowddata = _quicksort(crowddata, left)
+    crowddata = _quicksort(crowddata, right)
+    object_list[:] = left+[pivot]+right
+
+    return crowddata
+
+/* customized for using an original with comparisons for quicksort list */
+def quicksort(object_list, table_name, custom_presenter, custom_map_func, orig_img, seed, cc):
+    random.seed(seed)  # This is to gaurantee that quicksort is determinintic.
+    crowddata = cc.CrowdData([], table_name = table_name) \
+                            .set_presenter(custom_presenter, custom_map_func)
+    _quicksort(crowddata, object_list, orig_img)
+
+
+if __name__ == "__main__":
+    cc = CrowdContext()
+
+    object_list = ["http://www.kidsmathgamesonline.com/images/pictures/numbers600/number3.jpg",
+            "http://www.kidsmathgamesonline.com/images/pictures/numbers600/number4.jpg",
+            "http://www.kidsmathgamesonline.com/images/pictures/numbers600/number2.jpg",
+            "http://www.kidsmathgamesonline.com/images/pictures/numbers600/number1.jpg",
+            "http://www.kidsmathgamesonline.com/images/pictures/numbers600/number5.jpg"]
+
+    cc.delete_table("quicksort")
+    print "Unsorted Data:"
+    print "\n".join(object_list)
+
+    quicksort(object_list, "quicksort", 1, cc)
+
+    print "Sorted Data:"
+    print "\n".join(object_list)
